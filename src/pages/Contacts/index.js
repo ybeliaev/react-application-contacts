8a307deb19a8264e6039f4f573aee01058b05439
Jorgen <ybeliaev@gmail.com>
2021-05-08 16:29:20 +0300

    refactor fetch create hook useContacts

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,34 +1,42 @@
 import React, { useState, useEffect } from "react";
 
-export function Contacts() {
-  const [contacts, setContacts] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+const useContacts = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); // otherwise the name will be undefined
   const [isError, setIsError] = useState(false);
 
   const URL = "https://randomuser.me/api/?results=200";
 
   useEffect(() => {
-    setIsLoading(true);
     const getContacts = async (url) => {
       try {
+        setIsLoading(true);
         const response = await fetch(url);
-        const { results } = await response.json();
-        setContacts(results);
-        setIsLoading(false);
+        const { results, error } = await response.json();
+        if (error) {
+          throw new Error(error);
+        }
+        setData(results);
         setIsError(false);
       } catch (e) {
-        setIsLoading(false);
         setIsError(true);
+        console.error(e);
+      } finally {
+        setIsLoading(false);
       }
     };
     getContacts(URL);
   }, []);
+  return { data, isLoading, isError };
+};
 
-  if (isLoading) {
+export function Contacts() {
+  const contacts = useContacts();
+  if (contacts.isLoading) {
     return <div>Loading..</div>;
   }
-  if (isError) {
+  if (contacts.isError) {
     return <div>Error!</div>;
   }
-  return <div>Contacts: {contacts[0].name.first}</div>;
+  return <div>Contacts: {contacts.data[0].name.first}</div>;
 }
