f23045280d701d1ac23a0a88f168878ed6694a0b
Jorgen <ybeliaev@gmail.com>
2021-05-08 14:14:20 +0300

    add prettier.json + fetch

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-export  function Contacts() {
-    return (
-        <div>
-            Contacts
-        </div>
-    )
+export function Contacts() {
+  const [contacts, setContacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const URL = "https://randomuser.me/api/?results=200";
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => {
+        setContacts(data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading..</div>;
+  }
+  if (isError) {
+    return <div>Error!</div>;
+  }
+  return <div>Contacts</div>;
 }
