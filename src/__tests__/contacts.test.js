22cbebc6317dbb009804d13183338625a77998cb
Jorgen <ybeliaev@gmail.com>
2021-05-18 13:59:27 +0300

    add json data to const users for mock

--- src/__tests__/contacts.test.js
+++ src/__tests__/contacts.test.js
@@ -4,7 +4,21 @@ import {
   screen,
   waitForElementToBeRemoved,
 } from "@testing-library/react";
+// src/mocks/handlers.js
+import { rest } from "msw";
+// src/mocks/server.js
+import { setupServer } from "msw/node";
+
 import { Contacts } from "../pages/Contacts";
+import { users } from "../constants/constants";
+
+const handlers = [
+  rest.get("https://randomuser.me/api/?results=20", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ results: users }));
+  }),
+];
+
+const server = setupServer(...handlers);
 
 test(`contacts get data success`, async () => {
   render(<Contacts />);
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -22,3 +22,177 @@ export const DATA_VIEW_MODE = {
   TABLE: "table",
   GRID: "grid",
 };
+
+export const users = [
+  {
+    gender: "male",
+    name: {
+      title: "Monsieur",
+      first: "Armando",
+      last: "Olivier",
+    },
+    location: {
+      street: {
+        number: 9198,
+        name: "Avenue de la Libération",
+      },
+      city: "Fräschels",
+      state: "Fribourg",
+      country: "Switzerland",
+      postcode: 7192,
+      coordinates: {
+        latitude: "-48.3176",
+        longitude: "-144.1401",
+      },
+      timezone: {
+        offset: "+3:30",
+        description: "Tehran",
+      },
+    },
+    email: "armando.olivier@example.com",
+    login: {
+      uuid: "1c9e044c-3701-49ad-b61d-f0afbd9dc0fb",
+      username: "bigswan965",
+      password: "gabber",
+      salt: "lDbgMN1p",
+      md5: "855689526efbf57ff937edbbd0bb0a30",
+      sha1: "ac405cb77a8f3407387b323bea9b0aa89df7024d",
+      sha256:
+        "a93dc4a152beee713c6fff5a854c350941336ffb37f42c3d5ab7ad66e6da1782",
+    },
+    dob: {
+      date: "1945-02-25T06:38:15.818Z",
+      age: 76,
+    },
+    registered: {
+      date: "2013-10-20T07:24:40.827Z",
+      age: 8,
+    },
+    phone: "079 687 09 76",
+    cell: "079 250 95 03",
+    id: {
+      name: "AVS",
+      value: "756.6466.0143.68",
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/men/95.jpg",
+      medium: "https://randomuser.me/api/portraits/med/men/95.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/men/95.jpg",
+    },
+    nat: "CH",
+  },
+  {
+    gender: "female",
+    name: {
+      title: "Mrs",
+      first: "Lea",
+      last: "Jørgensen",
+    },
+    location: {
+      street: {
+        number: 5346,
+        name: "Rosengade",
+      },
+      city: "Esbjerg V",
+      state: "Hovedstaden",
+      country: "Denmark",
+      postcode: 81416,
+      coordinates: {
+        latitude: "19.2072",
+        longitude: "135.0745",
+      },
+      timezone: {
+        offset: "-11:00",
+        description: "Midway Island, Samoa",
+      },
+    },
+    email: "lea.jorgensen@example.com",
+    login: {
+      uuid: "490f805a-7aa0-4265-ae5e-85908a33cfc4",
+      username: "whiteladybug874",
+      password: "freeman",
+      salt: "9XsrraDH",
+      md5: "1a630a0bb80c8a98a67bc4d2a5f4d84d",
+      sha1: "7d9895f8f1b9eeb0ad6ce82c4933e96eb2ec6475",
+      sha256:
+        "9f5ebb10bd620ffb9b12bd88618c99255981735d2e628ac8fe89607a670741ec",
+    },
+    dob: {
+      date: "1953-04-19T18:46:54.290Z",
+      age: 68,
+    },
+    registered: {
+      date: "2017-08-05T21:25:06.674Z",
+      age: 4,
+    },
+    phone: "75577007",
+    cell: "55372251",
+    id: {
+      name: "CPR",
+      value: "190453-5040",
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/women/55.jpg",
+      medium: "https://randomuser.me/api/portraits/med/women/55.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/women/55.jpg",
+    },
+    nat: "DK",
+  },
+  {
+    gender: "female",
+    name: {
+      title: "Mrs",
+      first: "Izzie",
+      last: "Lopez",
+    },
+    location: {
+      street: {
+        number: 6839,
+        name: "Herbert Road",
+      },
+      city: "Enniscorthy",
+      state: "Sligo",
+      country: "Ireland",
+      postcode: 76442,
+      coordinates: {
+        latitude: "-38.7356",
+        longitude: "-75.9315",
+      },
+      timezone: {
+        offset: "-1:00",
+        description: "Azores, Cape Verde Islands",
+      },
+    },
+    email: "izzie.lopez@example.com",
+    login: {
+      uuid: "0785e7d2-e9c9-48bb-91c9-95b339280609",
+      username: "bluezebra687",
+      password: "torino",
+      salt: "P53tQ6jS",
+      md5: "6bd3f71297c8dc64ec1ed2d028ce48a9",
+      sha1: "2437775f96bb15594f1416d992aca83e4f48cbfa",
+      sha256:
+        "1bc37cd0b2d8959c34b3e0c43754f4df24459fdcd51d48888cf1d14d75f96019",
+    },
+    dob: {
+      date: "1951-03-30T20:07:13.719Z",
+      age: 70,
+    },
+    registered: {
+      date: "2015-02-27T22:19:05.507Z",
+      age: 6,
+    },
+    phone: "071-516-2942",
+    cell: "081-535-2546",
+    id: {
+      name: "PPS",
+      value: "0780684T",
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/women/41.jpg",
+      medium: "https://randomuser.me/api/portraits/med/women/41.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/women/41.jpg",
+    },
+    nat: "IE",
+  },
+];
